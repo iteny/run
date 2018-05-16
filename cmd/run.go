f214452b2ad8c5a2f5c2ce6e2b776cf2b2633e7b
NicholasMars <iteny@icloud.com>
2018-05-16 18:52:17 +0800

    no message

--- cmd/run.go
+++ cmd/run.go
@@ -17,17 +17,17 @@ var (
 )
 var Cfg struct {
 	Run struct {
-		InitCmds         [][]string       `toml:"init_cmds"`
-		WatchAll         bool             `toml:"watch_all"`
-		WatchDirs        []string         `toml:"watch_dirs"`
-		WatchExts        []string         `toml:"watch_exts"`
-		IgnoreDirs       []string         `toml:"ignore"`
-		IgnoreFiles      []string         `toml:"ignore_files"`
-		IgnoreRegexps    []*regexp.Regexp `toml:"-"`
-		BuildDelay       int              `toml:"build_delay"`
-		InterruptTimeout int              `toml:"interrupt_timout"`
-		GracefulKill     bool             `toml:"graceful_kill"`
-		Cmds             [][]string       `toml:"cmds"`
+		InitCmds         [][]string       `toml:"init_cmds"`        //初始化执行命令
+		WatchAll         bool             `toml:"watch_all"`        //监视所有子目录
+		WatchDirs        []string         `toml:"watch_dirs"`       //需要监视的目录
+		WatchExts        []string         `toml:"watch_exts"`       //需要监视的后缀
+		IgnoreDirs       []string         `toml:"ignore"`           //忽视的目录
+		IgnoreFiles      []string         `toml:"ignore_files"`     //忽视的文件
+		IgnoreRegexps    []*regexp.Regexp `toml:"-"`                //忽视的正则
+		BuildDelay       int              `toml:"build_delay"`      //事件触发最小间隔
+		InterruptTimeout int              `toml:"interrupt_timout"` //超时终止程序
+		GracefulKill     bool             `toml:"graceful_kill"`    //等待退出然后杀死
+		Cmds             [][]string       `toml:"cmds"`             //文件状态改变执行命令
 	} `toml:"run"`
 	Sync struct {
 		ListenAddr string `toml:"listen_addr"`
